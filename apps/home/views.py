fbc1e2eb76cd8818759974779d920f220b346374
Macktireh <abdimack97@gmail.com>
2023-02-20 00:27:59 +0100

    ajout de HomeViewTestCase pour tester la vue HomeView

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,13 +1,14 @@
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Mapping, Tuple
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+
 class HomeView(View):
-    
-    template_name = "home/index.html"
-    context = {}
-    
-    def get(self, request: HttpRequest, *args: Tuple, **kwargs: Dict[str, Any]) -> HttpResponse:
+
+    template_name: str = "home/index.html"
+    context: Mapping[str, Any] = {}
+
+    def get(self, request: HttpRequest, *args: Tuple[Any], **kwargs: Dict[str, Any]) -> HttpResponse:
         return render(request, self.template_name, self.context)
