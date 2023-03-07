034f84bcbb72a45f18ab01b5972ce8d41a3592e7
Macktireh <abdimack97@gmail.com>
2023-03-07 02:22:53 +0100

    ajout de l'application product et création des modèles Category, Product et des indexes sur les colonnes (public_id, created_at, updated_at). Egalement quelque amélioration de code comme ajout des modèles abstracts (AbstractPublicIdMixin, AbstractCreatedUpdatedMixin), changement de nom des tables explicite,

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -9,13 +9,13 @@ from apps.users.models import User
 class UserAdmin(BaseUserAdmin):
 
     model = User
-    list_display = ('email', 'first_name', 'last_name', 'phone_number', 'is_active', 'is_staff', 'is_superuser', 'date_joined',)
-    list_filter = ('is_superuser', 'is_staff', 'is_active', 'date_joined',)
+    list_display = ('email', 'first_name', 'last_name', 'phone_number', 'is_active', 'is_verified', 'is_staff', 'is_superuser', 'date_joined',)
+    list_filter = ('is_superuser', 'is_staff', 'is_verified', 'is_active', 'date_joined',)
     fieldsets = (
         (None, {'fields': ('email', 'password',)}),
         (_('Personal info'), {'fields': ('public_id','first_name', 'last_name', 'phone_number')}),
         (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'fields': ('is_active', 'is_verified', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         (_('Important dates'), {'fields': ('date_joined', 'last_login',)}),
     )
@@ -23,7 +23,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('first_name', 'last_name', 'phone_number', 'email', 'password1', 'password2', 'is_active', 'is_staff')}
+            'fields': ('first_name', 'last_name', 'phone_number', 'email', 'password1', 'password2', 'is_active', 'is_verified', 'is_staff')}
         ),
     )
     readonly_fields = ('public_id', 'date_joined', 'last_login')
--- apps/users/managers.py
+++ apps/users/managers.py
@@ -20,12 +20,15 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email: str, password: str, **extra_fields) -> Any:
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_verified', True)
         extra_fields.setdefault('is_staff',  True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_active') is not True:
             raise ValueError(_('Superuser must have is_active=True.'))
+        if extra_fields.get('is_verified') is not True:
+            raise ValueError(_('Superuser must have is_verified=True.'))
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
